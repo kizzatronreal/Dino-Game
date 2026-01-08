77ba6631f38072ea7b8373fdbc102f465ca1b6dd
= <=>
2026-01-08 13:34:39 -0500

    Created basic logic flow

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
-    let getRows = document.querySelectorAll('tr');
-    let getBlocks = document.querySelectorAll('td');
-    let rows = getData(getRows);
-    let blocks = getData(getBlocks);
+    let getRows1 = document.getElementsByClassName("row1");
+    let getBlocks1 = document.getElementsByClassName("block1");
+    let getRows2 = document.getElementsByClassName("row2");
+    let getBlocks2 = document.getElementsByClassName("block2");
 
-    
+    let row1 = getData(getRows1);
+    let blocks1 = getData(getBlocks1);
+    let row2 = getData(getRows2);
+    let blocks2 = getData(getBlocks2);
+
+
+
 })
 
 
@@ -24,4 +30,14 @@ function moveDino(x, y){
             
         }
     })
+}
+
+function createObstacle(row, dino){
+    for (let i = 0; i < row.length; i++){
+        
+    }
+}
+
+function spawnDino(){
+    
 }
